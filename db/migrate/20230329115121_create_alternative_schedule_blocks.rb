c9b5948946c20390ea7e83d997006eb972d9f463
Sane905 <konjaku0905@yahoo.co.jp>
2023-03-29 21:26:11 +0900

    feat:テーブル追加

--- db/migrate/20230329115121_create_alternative_schedule_blocks.rb
+++ db/migrate/20230329115121_create_alternative_schedule_blocks.rb
@@ -1,6 +1,9 @@
 class CreateAlternativeScheduleBlocks < ActiveRecord::Migration[6.1]
   def change
-    create_table :alternative_schedule_blocks do |t|
+    create_table :alternative_schedule_blocks, id: false do |t|
+      t.string :uid, null: false, primary_key: true
+      t.integer :capacity, null:false
+      t.integer :lock_version, default: 0, null: false
 
       t.timestamps
     end
