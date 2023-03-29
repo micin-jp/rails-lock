d91b79208410b60400f17838183df40db25d820d
Sane905 <konjaku0905@yahoo.co.jp>
2023-03-29 21:25:30 +0900

    feat:悲観的ロックの処理を追加

--- app/controllers/schedule_blocks_controller.rb
+++ app/controllers/schedule_blocks_controller.rb
@@ -1,2 +1,35 @@
 class ScheduleBlocksController < ApplicationController
+    def lock_transaction
+        block = ScheduleBlock.lock.first
+
+        ScheduleBlock.transaction do
+            if block.capacity == 0
+                render json: {message: '予約枠はありません', data:{capacity: block.capacity}}, status: :bad_request and return
+            end
+            block.capacity -= 1
+            block.save!
+            sleep(6)
+        end
+
+        render json: {message: '予約完了', data:{capacity: block.capacity}}, status: :ok
+    end
+
+    def not_lock_transaction
+    pp 000
+       block = ScheduleBlock.first
+       pp block
+       ScheduleBlock.transaction do
+        sleep(6)
+       pp 11
+        if block.capacity == 0
+            render json: {message: '予約枠はありません', data:{capacity: block.capacity}}, status: :bad_request and return
+        end
+
+         block.capacity -= 1
+         block.save!
+         sleep(6)
+       end
+
+       render json: {message: '予約完了', data:{capacity: block.capacity}}, status: :ok
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :schedule_blocks, only: [] do
+    post :lock_transaction
+    post :not_lock_transaction
+  end
+
+  resource :alternative_schedule_blocks, only: [] do
+    post :lock_transaction
+  end
 end
