35737535451669ef3df2778931fcff97d9419936
Sane905 <konjaku0905@yahoo.co.jp>
2023-03-29 21:25:53 +0900

    feat:楽観的ロックの処理の追加

--- app/controllers/alternative_schedule_blocks_controller.rb
+++ app/controllers/alternative_schedule_blocks_controller.rb
@@ -1,2 +1,16 @@
 class AlternativeScheduleBlocksController < ApplicationController
+    def lock_transaction
+        begin
+            block = AlternativeScheduleBlock.first
+            AlternativeScheduleBlock.transaction do
+              block.capacity -= 1
+              block.save!
+              sleep(6)
+            end
+        rescue ActiveRecord::StaleObjectError
+           render json: {message: '予約が失敗しました'}, status: :internal_server_error and return
+        end
+
+       render json: {message: '予約完了', data:{capacity: block.capacity}}, status: :ok
+    end
 end
--- app/models/alternative_schedule_block.rb
+++ app/models/alternative_schedule_block.rb
@@ -4,3 +4,14 @@ class AlternativeScheduleBlock < ApplicationRecord
       self.uid = Digest::UUID.uuid_v4.tr('-', '')
     end
 end
+
+# == Schema Information
+#
+# Table name: alternative_schedule_blocks
+#
+#  capacity     :integer          not null
+#  lock_version :integer          default(0), not null
+#  uid          :string           not null, primary key
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
